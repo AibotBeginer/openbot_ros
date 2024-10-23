70e634d58079a50f3ecfa758da7bc564e4112354
duyongquan <quandy2020@126.com>
2024-10-23 16:09:33 +0800

    [Fix] add messages_conversion
    	1 add messages_conversion
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ openbot_ros/include/messages_conversion/proto_msgs_to_ros.hpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2024 The OpenRobotic Beginner Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OPENBOT_ROS_OPENBOT_PROTO_MSGS_TO_ROS_HPP
+#define OPENBOT_ROS_OPENBOT_PROTO_MSGS_TO_ROS_HPP
+
+#include <string>
+#include <tuple>
+
+#include "rclcpp/rclcpp.hpp"
+// #include "openbot/common/proto/builtin_interfaces/time.pb.h"
+#include "openbot/common/port.hpp"
+
+namespace openbot_ros {
+
+// rclcpp::Time ToRos(const ::openbot::common::proto::builtin_interfaces::Time& time);
+
+}  // namespace openbot_ros
+
+#endif  // OPENBOT_ROS_OPENBOT_PROTO_MSGS_TO_ROS_HPP
