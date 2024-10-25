84fa2bf9e76a60489f2f62363b8969db1c415ea4
duyongquan <quandy2020@126.com>
2024-10-25 13:48:23 +0800

    [Add] Add messages_conversion
    	1 proto 2 ros msgs
    
    Signed-off-by: duyongquan <quandy2020@126.com>

--- openbot_ros/include/messages_conversion/proto_msgs_to_ros.hpp
+++ openbot_ros/include/messages_conversion/proto_msgs_to_ros.hpp
@@ -20,9 +20,31 @@
 #include <string>
 #include <tuple>
 
-// geometry_msgs
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/header.hpp"
+#include "openbot/common/port.hpp"
+
+// geometry_msgs
+#include "geometry_msgs/msg/accel.hpp"
+#include "geometry_msgs/msg/accel_stamped.hpp"
+#include "geometry_msgs/msg/accel_with_covariance.hpp"
+#include "geometry_msgs/msg/accel_with_covariance_stamped.hpp"
+#include "geometry_msgs/msg/inertia.hpp"
+#include "geometry_msgs/msg/inertia_stamped.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/point32.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+#include "geometry_msgs/msg/polygon.hpp"
+#include "geometry_msgs/msg/polygon_stamped.hpp"
+#include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/pose2_d.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_with_covariance.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
+#include "geometry_msgs/msg/quaternion_stamped.hpp"
+#include "geometry_msgs/msg/transform.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
 #include "geometry_msgs/msg/twist_with_covariance.hpp" 
@@ -30,6 +52,14 @@
 #include "geometry_msgs/msg/vector3.hpp"
 #include "geometry_msgs/msg/vector3_stamped.hpp"
 #include "geometry_msgs/msg/velocity_stamped.hpp"
+
+// nav_msgs
+#include "nav_msgs/msg/grid_cells.hpp"
+#include "nav_msgs/msg/map_meta_data.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "nav_msgs/msg/path.hpp"
+
 
 // sensor_msgs
 #include "sensor_msgs/msg/camera_info.hpp"
@@ -45,11 +75,56 @@
 #include "sensor_msgs/msg/range.hpp"
 #include "sensor_msgs/msg/region_of_interest.hpp"
 
+// shape_msgs
+
+
+// std_msgs
+#include "std_msgs/msg/header.hpp"
+
+// vision_msgs
+#include "vision_msgs/msg/bounding_box2_d.hpp"
+#include "vision_msgs/msg/bounding_box3_d.hpp"
+#include "vision_msgs/msg/bounding_box2_d_array.hpp"
+#include "vision_msgs/msg/bounding_box3_d_array.hpp"
+#include "vision_msgs/msg/classification.hpp"
+#include "vision_msgs/msg/detection2_d.hpp"
+#include "vision_msgs/msg/detection2_d_array.hpp"
+#include "vision_msgs/msg/detection3_d.hpp"
+#include "vision_msgs/msg/detection3_d_array.hpp"
+#include "vision_msgs/msg/label_info.hpp"
+#include "vision_msgs/msg/object_hypothesis.hpp"
+#include "vision_msgs/msg/object_hypothesis_with_pose.hpp"
+#include "vision_msgs/msg/point2_d.hpp"
+#include "vision_msgs/msg/pose2_d.hpp"
+#include "vision_msgs/msg/vision_class.hpp"
+#include "vision_msgs/msg/vision_info.hpp"
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
 // geometry_msgs
-#include "openbot/common/port.hpp"
-#include "openbot/common/proto/std_msgs/header.pb.h"
+#include "openbot/common/proto/geometry_msgs/accel.pb.h"  
+#include "openbot/common/proto/geometry_msgs/accel_stamped.pb.h"  
+#include "openbot/common/proto/geometry_msgs/accel_with_covariance.pb.h"
+#include "openbot/common/proto/geometry_msgs/accel_with_covariance_stamped.pb.h"  
+#include "openbot/common/proto/geometry_msgs/inertia.pb.h"  
+#include "openbot/common/proto/geometry_msgs/inertia_stamped.pb.h"  
+#include "openbot/common/proto/geometry_msgs/point.pb.h"  
+#include "openbot/common/proto/geometry_msgs/point32.pb.h"  
+#include "openbot/common/proto/geometry_msgs/point_stamped.pb.h"  
+#include "openbot/common/proto/geometry_msgs/polygon.pb.h"  
+#include "openbot/common/proto/geometry_msgs/polygon_stamped.pb.h"
+#include "openbot/common/proto/geometry_msgs/pose.pb.h"  
+#include "openbot/common/proto/geometry_msgs/pose_2d.pb.h"  
+#include "openbot/common/proto/geometry_msgs/pose_array.pb.h"  
+#include "openbot/common/proto/geometry_msgs/pose_stamped.pb.h"
+#include "openbot/common/proto/geometry_msgs/pose_with_covariance.pb.h"
+#include "openbot/common/proto/geometry_msgs/pose_with_covariance_stamped.pb.h"
+#include "openbot/common/proto/geometry_msgs/quaternion.pb.h"  
+#include "openbot/common/proto/geometry_msgs/quaternion_stamped.pb.h"
+#include "openbot/common/proto/geometry_msgs/transform.pb.h"  
+#include "openbot/common/proto/geometry_msgs/transform_stamped.pb.h"
 #include "openbot/common/proto/geometry_msgs/twist.pb.h"  
 #include "openbot/common/proto/geometry_msgs/twist_stamped.pb.h"  
 #include "openbot/common/proto/geometry_msgs/twist_with_covariance.pb.h"
@@ -59,6 +134,13 @@
 #include "openbot/common/proto/geometry_msgs/velocity_stamped.pb.h" 
 #include "openbot/common/proto/builtin_interfaces/time.pb.h" 
 #include "openbot/common/proto/builtin_interfaces/duration.pb.h"
+
+// nav_msgs
+#include "openbot/common/proto/nav_msgs/grid_cells.pb.h" 
+#include "openbot/common/proto/nav_msgs/map_meta_data.pb.h"
+#include "openbot/common/proto/nav_msgs/occupancy_grid.pb.h" 
+#include "openbot/common/proto/nav_msgs/odometry.pb.h"
+#include "openbot/common/proto/nav_msgs/path.pb.h" 
 
 // sensor_msgs
 #include "openbot/common/proto/sensor_msgs/camera_info.pb.h" 
@@ -74,6 +156,30 @@
 #include "openbot/common/proto/sensor_msgs/range.pb.h"
 #include "openbot/common/proto/sensor_msgs/region_of_interest.pb.h"
 
+// shape_msgs
+
+
+// std_msgs
+#include "openbot/common/proto/std_msgs/header.pb.h"
+
+// vision_msgs
+#include "openbot/common/proto/vision_msgs/bounding_box_2d.pb.h"
+#include "openbot/common/proto/vision_msgs/bounding_box_3d.pb.h"
+#include "openbot/common/proto/vision_msgs/bounding_box_2d_array.pb.h"
+#include "openbot/common/proto/vision_msgs/bounding_box_3d_array.pb.h"
+#include "openbot/common/proto/vision_msgs/classification.pb.h"
+#include "openbot/common/proto/vision_msgs/detection_2d.pb.h"
+#include "openbot/common/proto/vision_msgs/detection_2d_array.pb.h"
+#include "openbot/common/proto/vision_msgs/detection_3d_array.pb.h"
+#include "openbot/common/proto/vision_msgs/detection_3d_array.pb.h"
+#include "openbot/common/proto/vision_msgs/label_info.pb.h"
+#include "openbot/common/proto/vision_msgs/object_hypothesis.pb.h"
+#include "openbot/common/proto/vision_msgs/object_hypothesis_with_pose.pb.h"
+#include "openbot/common/proto/vision_msgs/point_2d.pb.h"
+#include "openbot/common/proto/vision_msgs/pose_2d.pb.h"
+#include "openbot/common/proto/vision_msgs/vision_class.pb.h"
+#include "openbot/common/proto/vision_msgs/vision_info.pb.h"
+
 namespace openbot_ros {
 
 //------------------------------------------ builtin_interfaces -------------------------------------
@@ -82,6 +188,41 @@ namespace openbot_ros {
 rclcpp::Time ToRos(const ::openbot::common::proto::builtin_interfaces::Time& proto);
 
 //------------------------------------------ geometry_msgs ------------------------------------------
+
+// Accel
+geometry_msgs::msg::Accel ToRos(const ::openbot::common::proto::geometry_msgs::Accel& proto);
+geometry_msgs::msg::AccelStamped ToRos(const ::openbot::common::proto::geometry_msgs::AccelStamped& proto);
+geometry_msgs::msg::AccelWithCovariance ToRos(const ::openbot::common::proto::geometry_msgs::AccelWithCovariance& proto);
+geometry_msgs::msg::AccelWithCovarianceStamped ToRos(const ::openbot::common::proto::geometry_msgs::AccelWithCovarianceStamped& proto);
+
+// Inertia
+geometry_msgs::msg::Inertia ToRos(const ::openbot::common::proto::geometry_msgs::Inertia& proto);
+geometry_msgs::msg::InertiaStamped ToRos(const ::openbot::common::proto::geometry_msgs::InertiaStamped& proto);
+
+// Point
+geometry_msgs::msg::Point ToRos(const ::openbot::common::proto::geometry_msgs::Point& proto);
+geometry_msgs::msg::Point32 ToRos(const ::openbot::common::proto::geometry_msgs::Point32& proto);
+geometry_msgs::msg::PointStamped ToRos(const ::openbot::common::proto::geometry_msgs::PointStamped& proto);
+
+// polygon
+geometry_msgs::msg::Polygon ToRos(const ::openbot::common::proto::geometry_msgs::Polygon& proto);
+geometry_msgs::msg::PolygonStamped ToRos(const ::openbot::common::proto::geometry_msgs::PolygonStamped& proto);
+
+// Pose
+geometry_msgs::msg::Pose ToRos(const ::openbot::common::proto::geometry_msgs::Pose& proto);
+geometry_msgs::msg::Pose2D ToRos(const ::openbot::common::proto::geometry_msgs::Pose2D& proto);
+geometry_msgs::msg::PoseArray ToRos(const ::openbot::common::proto::geometry_msgs::PoseArray& proto);
+geometry_msgs::msg::PoseStamped ToRos(const ::openbot::common::proto::geometry_msgs::PoseStamped& proto);
+geometry_msgs::msg::PoseWithCovariance ToRos(const ::openbot::common::proto::geometry_msgs::PoseWithCovariance& proto);
+geometry_msgs::msg::PoseWithCovarianceStamped ToRos(const ::openbot::common::proto::geometry_msgs::PoseWithCovarianceStamped& proto);
+
+// Quaternion
+geometry_msgs::msg::Quaternion ToRos(const ::openbot::common::proto::geometry_msgs::Quaternion& proto);
+geometry_msgs::msg::QuaternionStamped ToRos(const ::openbot::common::proto::geometry_msgs::QuaternionStamped& proto);
+
+// transform
+geometry_msgs::msg::Transform ToRos(const ::openbot::common::proto::geometry_msgs::Transform& proto);
+geometry_msgs::msg::TransformStamped ToRos(const ::openbot::common::proto::geometry_msgs::TransformStamped& proto);
 
 // Twist
 geometry_msgs::msg::Twist ToRos(const ::openbot::common::proto::geometry_msgs::Twist& proto);
@@ -95,6 +236,21 @@ geometry_msgs::msg::Vector3Stamped ToRos(const ::openbot::common::proto::geometr
 geometry_msgs::msg::VelocityStamped ToRos(const ::openbot::common::proto::geometry_msgs::VelocityStamped& proto);
 
 //------------------------------------------ nav_msgs ------------------------------------------
+
+// GridCells
+nav_msgs::msg::GridCells ToRos(const ::openbot::common::proto::nav_msgs::GridCells& proto);
+
+// MapMetaData
+nav_msgs::msg::MapMetaData ToRos(const ::openbot::common::proto::nav_msgs::MapMetaData& proto);
+
+// OccupancyGrid
+nav_msgs::msg::OccupancyGrid ToRos(const ::openbot::common::proto::nav_msgs::OccupancyGrid& proto);
+
+// Odometry
+nav_msgs::msg::Odometry ToRos(const ::openbot::common::proto::nav_msgs::Odometry& proto);
+
+// Path
+nav_msgs::msg::Path ToRos(const ::openbot::common::proto::nav_msgs::Path& proto);
 
 
 //------------------------------------------ sensor_msgs ------------------------------------------
--- openbot_ros/src/messages_conversion/proto_msgs_to_ros.cpp
+++ openbot_ros/src/messages_conversion/proto_msgs_to_ros.cpp
@@ -30,7 +30,197 @@ std_msgs::msg::Header ToRos(const ::openbot::common::proto::std_msgs::Header& pr
     data.stamp = ToRos(proto.stamp());
     data.frame_id = proto.frame_id();
     return data;
-}   
+}  
+
+geometry_msgs::msg::Accel ToRos(const ::openbot::common::proto::geometry_msgs::Accel& proto)
+{
+    geometry_msgs::msg::Accel data;
+    data.linear = ToRos(proto.linear());
+    data.angular = ToRos(proto.angular());
+    return data;
+}
+
+geometry_msgs::msg::AccelStamped ToRos(const ::openbot::common::proto::geometry_msgs::AccelStamped& proto)
+{
+    geometry_msgs::msg::AccelStamped data;
+    data.header = ToRos(proto.header());
+    data.accel = ToRos(proto.accel());
+    return data;
+}
+
+geometry_msgs::msg::AccelWithCovariance ToRos(const ::openbot::common::proto::geometry_msgs::AccelWithCovariance& proto)
+{
+    geometry_msgs::msg::AccelWithCovariance data;
+    data.accel = ToRos(proto.accel());
+
+    for (int i = 0; i < proto.covariance_size(); ++i) {
+        data.covariance[i] = proto.covariance(i);
+    }
+    return data;
+}
+
+geometry_msgs::msg::AccelWithCovarianceStamped ToRos(
+    const ::openbot::common::proto::geometry_msgs::AccelWithCovarianceStamped& proto)
+{
+    geometry_msgs::msg::AccelWithCovarianceStamped data;
+    data.header = ToRos(proto.header());
+    data.accel = ToRos(proto.accel());
+    return data;
+}
+
+geometry_msgs::msg::Inertia ToRos(const ::openbot::common::proto::geometry_msgs::Inertia& proto)
+{
+    geometry_msgs::msg::Inertia data;
+    data.m = proto.m();
+    data.com = ToRos(proto.com());
+    data.ixx = proto.ixx();
+    data.ixy = proto.ixy();
+    data.ixz = proto.ixz();
+    data.iyy = proto.iyy();
+    data.iyz = proto.iyz();
+    data.izz = proto.izz();
+    return data;
+}
+
+geometry_msgs::msg::InertiaStamped ToRos(const ::openbot::common::proto::geometry_msgs::InertiaStamped& proto)
+{
+    geometry_msgs::msg::InertiaStamped data;
+    data.header = ToRos(proto.header());
+    data.inertia = ToRos(proto.inertia());
+    return data;
+}
+
+geometry_msgs::msg::Point ToRos(const ::openbot::common::proto::geometry_msgs::Point& proto)
+{
+    geometry_msgs::msg::Point data;
+    data.x = proto.x();
+    data.y = proto.y();
+    data.z = proto.z();
+    return data;
+}
+
+geometry_msgs::msg::Point32 ToRos(const ::openbot::common::proto::geometry_msgs::Point32& proto)
+{
+    geometry_msgs::msg::Point32 data;
+    data.x = proto.x();
+    data.y = proto.y();
+    data.z = proto.z();
+    return data;
+}
+
+geometry_msgs::msg::PointStamped ToRos(const ::openbot::common::proto::geometry_msgs::PointStamped& proto)
+{
+    geometry_msgs::msg::PointStamped data;
+    data.header = ToRos(proto.header());
+    data.point = ToRos(proto.point());
+    return data;
+}
+
+geometry_msgs::msg::Polygon ToRos(const ::openbot::common::proto::geometry_msgs::Polygon& proto)
+{
+    geometry_msgs::msg::Polygon data;
+    for (int i = 0 ; i < proto.points_size(); ++i) {
+        data.points[i] = ToRos(proto.points(i));
+    }
+    return data;
+}
+
+geometry_msgs::msg::PolygonStamped ToRos(const ::openbot::common::proto::geometry_msgs::PolygonStamped& proto)
+{
+    geometry_msgs::msg::PolygonStamped data;
+    data.header = ToRos(proto.header());
+    data.polygon = ToRos(proto.polygon());
+    return data;
+}
+
+geometry_msgs::msg::Pose ToRos(const ::openbot::common::proto::geometry_msgs::Pose& proto)
+{
+    geometry_msgs::msg::Pose data;
+    data.position = ToRos(proto.position());
+    data.orientation = ToRos(proto.orientation());
+    return data;
+}
+
+geometry_msgs::msg::Pose2D ToRos(const ::openbot::common::proto::geometry_msgs::Pose2D& proto)
+{
+    geometry_msgs::msg::Pose2D data;
+    data.x = proto.x();
+    data.y = proto.y();
+    data.theta = proto.theta();
+    return data;
+}
+
+geometry_msgs::msg::PoseArray ToRos(const ::openbot::common::proto::geometry_msgs::PoseArray& proto)
+{
+    geometry_msgs::msg::PoseArray data;
+    data.header = ToRos(proto.header());
+    for (int i = 0; i < proto.poses_size(); ++i) {
+        data.poses[i] = ToRos(proto.poses(i));
+    }
+    return data;
+}
+
+geometry_msgs::msg::PoseStamped ToRos(const ::openbot::common::proto::geometry_msgs::PoseStamped& proto)
+{
+    geometry_msgs::msg::PoseStamped data;
+    data.header = ToRos(proto.header());
+    data.pose = ToRos(proto.pose());
+    return data;
+}
+
+geometry_msgs::msg::PoseWithCovariance ToRos(const ::openbot::common::proto::geometry_msgs::PoseWithCovariance& proto)
+{
+    geometry_msgs::msg::PoseWithCovariance data;
+    data.pose = ToRos(proto.pose());
+    for (int i = 0; i < proto.covariance_size(); ++i) {
+        data.covariance[i] = proto.covariance(i);
+    }
+    return data;
+}
+
+geometry_msgs::msg::PoseWithCovarianceStamped ToRos(
+    const ::openbot::common::proto::geometry_msgs::PoseWithCovarianceStamped& proto)
+{
+    geometry_msgs::msg::PoseWithCovarianceStamped data;
+    data.header = ToRos(proto.header());
+    data.pose = ToRos(proto.pose());
+    return data;
+}
+
+geometry_msgs::msg::Quaternion ToRos(const ::openbot::common::proto::geometry_msgs::Quaternion& proto)
+{
+    geometry_msgs::msg::Quaternion data;
+    data.x = proto.x();
+    data.y = proto.y();
+    data.z = proto.z();
+    data.w = proto.w();
+    return data;
+}
+
+geometry_msgs::msg::QuaternionStamped ToRos(const ::openbot::common::proto::geometry_msgs::QuaternionStamped& proto)
+{
+    geometry_msgs::msg::QuaternionStamped data;
+    data.header = ToRos(proto.header());
+    data.quaternion = ToRos(proto.quaternion());
+    return data;
+}
+
+geometry_msgs::msg::Transform ToRos(const ::openbot::common::proto::geometry_msgs::Transform& proto)
+{
+    geometry_msgs::msg::Transform data;
+    data.translation = ToRos(proto.translation());
+    data.rotation = ToRos(proto.rotation());
+    return data;
+}
+
+geometry_msgs::msg::TransformStamped ToRos(const ::openbot::common::proto::geometry_msgs::TransformStamped& proto)
+{
+    geometry_msgs::msg::TransformStamped data;
+    data.header = ToRos(proto.header());
+    data.child_frame_id = proto.child_frame_id();
+    data.transform = ToRos(proto.transform());
+    return data;
+}
 
 geometry_msgs::msg::Twist ToRos(const ::openbot::common::proto::geometry_msgs::Twist& proto)
 {
@@ -69,7 +259,6 @@ geometry_msgs::msg::TwistWithCovariance ToRos(const ::openbot::common::proto::ge
 {
     geometry_msgs::msg::TwistWithCovariance data;
     data.twist = ToRos(proto.twist());
-
     for (int i = 0; i < proto.covariance_size(); ++i) {
         data.covariance[i] = proto.covariance(i);
     }
@@ -90,6 +279,60 @@ geometry_msgs::msg::VelocityStamped ToRos(const ::openbot::common::proto::geomet
     geometry_msgs::msg::VelocityStamped data;
     data.header = ToRos(proto.header());
     data.velocity = ToRos(proto.velocity());
+    return data;
+}
+
+nav_msgs::msg::GridCells ToRos(const ::openbot::common::proto::nav_msgs::GridCells& proto)
+{
+    nav_msgs::msg::GridCells data;
+    data.header = ToRos(proto.header());
+    data.cell_width = proto.cell_width();
+    data.cell_height = proto.cell_height();
+    for (int i = 0; i < proto.cells_size(); ++i) {
+        data.cells[i] = ToRos(proto.cells(i));
+    }
+    return data;
+}
+
+nav_msgs::msg::MapMetaData ToRos(const ::openbot::common::proto::nav_msgs::MapMetaData& proto)
+{
+    nav_msgs::msg::MapMetaData data;
+    data.map_load_time = ToRos(proto.map_load_time());
+    data.resolution = proto.resolution();
+    data.width = proto.width();
+    data.height = proto.height();
+    data.origin = ToRos(proto.origin());
+    return data;
+}
+
+nav_msgs::msg::OccupancyGrid ToRos(const ::openbot::common::proto::nav_msgs::OccupancyGrid& proto)
+{
+    nav_msgs::msg::OccupancyGrid data;
+    data.header = ToRos(proto.header());
+    data.info = ToRos(proto.info());
+    for (int i = 0; i < proto.data_size(); ++i) {
+        data.data[i] = proto.data(i);
+    }
+    return data;
+}
+
+nav_msgs::msg::Odometry ToRos(const ::openbot::common::proto::nav_msgs::Odometry& proto)
+{
+    nav_msgs::msg::Odometry data;
+    data.header = ToRos(proto.header());
+    data.child_frame_id = proto.child_frame_id();
+    data.pose = ToRos(proto.pose());
+    data.twist = ToRos(proto.twist());
+    return data;
+}
+
+nav_msgs::msg::Path ToRos(const ::openbot::common::proto::nav_msgs::Path& proto)
+{
+    nav_msgs::msg::Path data;
+    data.header = ToRos(proto.header());
+    for (int i = 0; i < proto.poses_size(); ++i) {
+        data.poses[i] = ToRos(proto.poses(i));
+    }
     return data;
 }
 
@@ -118,7 +361,7 @@ sensor_msgs::msg::CameraInfo ToRos(const ::openbot::common::proto::sensor_msgs::
 
     data.binning_x = proto.binning_x();
     data.binning_y = proto.binning_y();
-    // data.roi = ToRos(proto.roi());
+    data.roi = ToRos(proto.roi());
     return data;
 }
 
@@ -171,12 +414,12 @@ sensor_msgs::msg::Imu ToRos(const ::openbot::common::proto::sensor_msgs::Imu& pr
 {
     sensor_msgs::msg::Imu data;
     data.header = ToRos(proto.header());
-    // data.orientation = ToRos(proto.orientation());
+    data.orientation = ToRos(proto.orientation());
 
-    // for (int i = 0; i < proto.orientation_covariance_size(); ++i) {
-    //     data.orientation_covariance[i] = proto.orientation_covariance(i);
-    // }
-    // data.angular_velocity = ToRos(proto.angular_velocity());
+    for (int i = 0; i < proto.orientation_covariance_size(); ++i) {
+        data.orientation_covariance[i] = proto.orientation_covariance(i);
+    }
+    data.angular_velocity = ToRos(proto.angular_velocity());
     return data;
 }
 
@@ -208,13 +451,13 @@ sensor_msgs::msg::PointCloud ToRos(const ::openbot::common::proto::sensor_msgs::
     sensor_msgs::msg::PointCloud data;
     data.header = ToRos(proto.header());
 
-    // for (int i = 0; i < proto.points_size(); ++i) {
-    //     data.points[i] = proto.points(i);
-    // }
+    for (int i = 0; i < proto.points_size(); ++i) {
+        data.points[i] = ToRos(proto.points(i));
+    }
 
-    // for (int i = 0; i < proto.channels_size(); ++i) {
-    //     data.channels[i] = proto.channels(i);
-    // }
+    for (int i = 0; i < proto.channels_size(); ++i) {
+        data.channels[i] = ToRos(proto.channels(i));
+    }
     
     return data;
 }
@@ -225,9 +468,9 @@ sensor_msgs::msg::PointCloud2 ToRos(const ::openbot::common::proto::sensor_msgs:
     data.header = ToRos(proto.header());
     data.height = proto.height();
     data.width = proto.width();
-    // for (int i = 0; i < proto.fields_size(); ++i) {
-    //     data.fields[i] = proto.fields(i);
-    // }
+    for (int i = 0; i < proto.fields_size(); ++i) {
+        data.fields[i] = ToRos(proto.fields(i));
+    }
     data.is_bigendian = proto.is_bigendian();
     data.point_step = proto.point_step();
     for (int i = 0; i < proto.data_size(); ++i) {
@@ -269,5 +512,6 @@ sensor_msgs::msg::RegionOfInterest ToRos(const ::openbot::common::proto::sensor_
     data.do_rectify = proto.do_rectify();
     return data;
 }
+
 
 }  // namespace openbot_ros
