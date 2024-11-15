5c2ac0d90c83e9fda590e2a55eb79b2492c88505
edfus <edfusedfus@gmail.com>
2024-11-15 14:00:49 +0800

    [Feat] Dead reckoning fake node with static map to odom transform

--- openbot_ros/launch/demo_nav2_octomap_openbot.launch.py
+++ openbot_ros/launch/demo_nav2_octomap_openbot.launch.py
@@ -16,7 +16,11 @@
 """
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    ExecuteProcess,
+)
 from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, SetRemap
@@ -25,42 +29,218 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import Shutdown
 import os
 
+import os
+import tempfile
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+    RegisterEventHandler,
+)
+from launch.conditions import IfCondition
+from launch.event_handlers import OnShutdown
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, SetParameter, SetRemap
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import HasNodeParams, RewrittenYaml
+
+from launch_ros.actions import Node
+
+
 def generate_launch_description():
+    # Get the launch directory
+    nav2_bringup_dir = get_package_share_directory("nav2_bringup")
+    nav2_launch_dir = os.path.join(nav2_bringup_dir, "launch")
+    openbot_ros_dir = FindPackageShare("openbot_ros").find("openbot_ros")
+    sim_dir = get_package_share_directory("openbot_fake_robot")
 
     # Create the launch configuration variables
-    namespace = LaunchConfiguration('namespace')
-    use_namespace = LaunchConfiguration('use_namespace')
+    namespace = LaunchConfiguration("namespace")
+    use_namespace = LaunchConfiguration("use_namespace")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    params_file = LaunchConfiguration("params_file")
+    autostart = LaunchConfiguration("autostart")
+    use_composition = LaunchConfiguration("use_composition")
+    use_respawn = LaunchConfiguration("use_respawn")
 
-     # Declare the launch arguments
+    # Launch configuration variables specific to simulation
+    use_robot_state_pub = LaunchConfiguration("use_robot_state_pub")
+
+    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+
+    # Declare the launch arguments
     declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace',
-        default_value='navigation',
-        description=('Top-level namespace. The value will be used to replace the '
-                     '<robot_namespace> keyword on the rviz config file.'))
+        "namespace", default_value="", description="Top-level namespace"
+    )
 
     declare_use_namespace_cmd = DeclareLaunchArgument(
-        'use_namespace',
-        default_value='false',
-        description='Whether to apply a namespace to the navigation stack')
+        "use_namespace",
+        default_value="false",
+        description="Whether to apply a namespace to the navigation stack",
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=os.path.join(sim_dir, "param", "nav2_params.yaml"),
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
+    )
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart",
+        default_value="true",
+        description="Automatically startup the nav2 stack",
+    )
+
+    declare_use_composition_cmd = DeclareLaunchArgument(
+        "use_composition",
+        default_value="True",
+        description="Whether to use composed bringup",
+    )
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        "use_respawn",
+        default_value="False",
+        description="Whether to respawn if a node crashes. Applied when composition is disabled.",
+    )
+
+    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+        "use_robot_state_pub",
+        default_value="True",
+        description="Whether to start the robot state publisher",
+    )
+
+    urdf = os.path.join(sim_dir, "urdf", "turtlebot3_waffle.urdf")
+    with open(urdf, "r") as infp:
+        robot_description = infp.read()
+
+    start_robot_state_publisher_cmd = Node(
+        condition=IfCondition(use_robot_state_pub),
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        namespace=namespace,
+        output="screen",
+        parameters=[
+            {"use_sim_time": use_sim_time, "robot_description": robot_description}
+        ],
+        remappings=remappings,
+    )
+
+    start_navigation_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(nav2_launch_dir, "navigation_launch.py")
+        ),
+        launch_arguments={
+            "namespace": namespace,
+            "use_sim_time": use_sim_time,
+            "autostart": autostart,
+            "params_file": params_file,
+            "use_composition": use_composition,
+            "use_respawn": use_respawn
+        }.items()
+    )
+    
+    # In ROS 2, when launching a node, arguments should be passed as strings
+    start_static_map_odom_transform = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="screen",
+        arguments=["0", "0", "0", "0", "0", "0", "map", "odom"],
+    )
 
 
-    start_openbot_cmd = Node(
-        package = 'openbot_ros',
-        executable = 'openbot_node',
-        parameters = [{'use_sim_time': True}],
-        arguments = [
-            '-configuration_directory', FindPackageShare('openbot_ros').find('openbot_ros') + '/configuration_files',
-            '-configuration_basename', 'openbot.lua'],
-        # remappings = [
-        #     ('scan', 'horizontal_laser_2d')],
-        output = 'screen')
+    start_mockamap_cmd = Node(
+        package="openbot_ros",
+        executable="mockamap_generator_node",
+        parameters=[
+            {"use_sim_time": True},
+            {"seed": 511},
+            {"resolution": 0.1},
+            {"x_length": 15},
+            {"y_length": 15},
+            {"z_length": 1},
+            {"type": 2},
+            {"complexity": 0.03},
+            {"fill": 0.3},
+            {"fractal": 1},
+            {"attenuation": 0.1},
+            {"width_min": 0.6},
+            {"width_max": 1.5},
+            {"obstacle_number": 50},
+            {"road_width": 0.5},
+            {"add_wall_x": 0},
+            {"add_wall_y": 1},
+            {"maze_type": 1},
+            {"num_nodes": 40},
+            {"connectivity": 0.8},
+            {"node_rad": 1},
+            {"road_rad": 10},
+        ],
+        output="screen",
+    )
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites={},
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
+    start_nav2_map_server_cmd = Node(
+        package="nav2_map_server",
+        executable="map_server",
+        name="map_server",
+        output="screen",
+        respawn=use_respawn,
+        respawn_delay=2.0,
+        parameters=[configured_params],
+        arguments=["--ros-args", "--log-level", "info"],
+        remappings=remappings,
+    )
+
+    
+
+    start_fake_odom_simulator = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                sim_dir,
+                "launch",
+                "turtlebot3_fake_node.launch.py",
+            )
+        )
+    )
 
     start_rviz_cmd = Node(
-        package = 'rviz2',
-        executable = 'rviz2',
-        on_exit = Shutdown(),
-        arguments = ['-d', FindPackageShare('openbot_ros').find('openbot_ros') + '/rviz/openbot_ros.rviz'],
-        parameters = [{'use_sim_time': True}],
+        package="rviz2",
+        executable="rviz2",
+        on_exit=Shutdown(),
+        arguments=[
+            "-d",
+            os.path.join(openbot_ros_dir, "rviz/nav2_octomap_openbot.rviz"),
+        ],
+        parameters=[{"use_sim_time": True}],
     )
 
     # Create the launch description and populate
@@ -69,9 +249,23 @@ def generate_launch_description():
     # Declare the launch options
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_namespace_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_composition_cmd)
+
+    ld.add_action(declare_use_robot_state_pub_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+
+    # Add the actions to launch all of the navigation nodes
+    ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(start_navigation_cmd)
+    ld.add_action(start_static_map_odom_transform)
+    ld.add_action(start_fake_odom_simulator)
+    ld.add_action(start_nav2_map_server_cmd)
 
     # Add any conditioned actions
     ld.add_action(start_rviz_cmd)
-    ld.add_action(start_openbot_cmd)
+    # ld.add_action(start_mockamap_cmd)
 
     return ld
