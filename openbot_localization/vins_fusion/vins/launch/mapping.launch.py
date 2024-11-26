618434bd9a63c700a71c5e8255aced2fa8e11825
duyongquan <quandy2020@126.com>
2024-11-26 14:19:04 +0800

    [Fix] Add vins fusion
    	1 vins
    
    Signed-off-by: duyongquan <quandy2020@126.com>

---
+++ openbot_localization/vins_fusion/vins/launch/mapping.launch.py
@@ -0,0 +1,74 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='navigation',
+        description=('Top-level namespace. The value will be used to replace the '
+                     '<robot_namespace> keyword on the rviz config file.'))
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='false',
+        description='Whether to apply a namespace to the navigation stack')
+
+
+    params_file = os.path.join(
+        get_package_share_directory('vins'),
+        'params',
+        'vins_fusion.yaml')
+
+    # Specify the actions
+    start_vins_cmd = Node(
+        package='vins',
+        executable='vins_node',
+        parameters = [params_file],
+        output='screen'
+    )
+
+    # For fake localization we need static transform between /odom and /map 
+    camera_link_to_base_link_cmd = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'camera_link'])
+
+
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_namespace_cmd)
+
+    # Add any conditioned actions
+    ld.add_action(start_vins_cmd)
+    # ld.add_action(camera_link_to_base_link_cmd)
+
+    return ld
